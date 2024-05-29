619e0208ceafc4b16aeaeedf9305abaeba1b623c
YunoGH <89008852+YunoGH@users.noreply.github.com>
2024-05-29 10:58:11 +0200

    branch push

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppMAD24Theme {
-                // A surface container using the 'background' color from the theme
+                // A surface container using the 'background' color from the theme.
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
