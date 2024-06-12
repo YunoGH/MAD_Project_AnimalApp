b658d3b14d45e1f551971762bf53986070a2153c
Are <maksimovic34@gmail.com>
2024-06-12 16:19:02 +0200

    Screens/Navigation added

--- app/src/main/java/com/example/animalApp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/animalApp/screens/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.example.animalApp.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,13 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.animalApp.R
-import com.example.animalApp.ui.theme.PurpleGrey40
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
